47d8e58317d991d4c92aada391a43dc58e4f81aa
Henrique Brandão <hb06ars@hotmail.com>
2023-07-23 20:46:57 -0300

    a
    a

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,11 +6,50 @@ video {
 canvas {
   display: none;
 }
+
+#customers {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: white;
+}
 </style>
 
 <template>
-  <div class="row items-center" style="height: 70vh">
-    
+  <div id="excel" class="row items-center" style="height: 20vh; padding:10px; display:none">
+    <div class="col-12 text-center" style="font-size:30px;">    
+      <table id="customers" style="border: 1 solid;  overflow:auto"></table>
+      &nbsp;
+    </div>
+    <div class="col-12 text-center">    
+        <q-btn id="voltar" @click="baixar()" color="primary" icon="download" label="BAIXAR" class="full-width" size="lg"/>
+        &nbsp;
+        
+        <q-btn id="voltar" @click="voltar()" color="primary" icon="home" label="VOLTAR" class="full-width" size="lg"/>
+        &nbsp;
+        <q-btn id="removertudo" @click="removertudo()" color="secondary" icon="delete" label="REMOVER TUDO" class="full-width" size="lg"/>
+    </div>
+  </div>
+
+
+
+  <div id="pagina" class="row items-center" style="height: 70vh">
       <div id="div_esconde" class="col-12 text-center" style="font-size:30px;">
         &nbsp;
         <div class="col-6 text-center">
@@ -50,7 +89,7 @@ canvas {
       <q-btn id="btsalvar" @click="salvar()" color="primary" icon="save" label="SALVAR"
         class="full-width" size="lg"/>
       &nbsp;
-      <q-btn id="download" @click="baixar()" color="secondary" icon="download" label="BAIXAR"
+      <q-btn id="download" @click="ver()" color="secondary" icon="download" label="VER ITENS"
         class="full-width" size="lg"/>
     </div>
   </div>
@@ -120,6 +159,10 @@ import Quagga from 'quagga'
         localStorage.setItem("lista", JSON.stringify(lista));
 
         alert('PRODUTO SALVO COM SUCESSO! ')
+        document.getElementById("produto").value = '';
+        document.getElementById("ean").value = '';
+        document.getElementById("valor").value = '';
+
         
       },
       esconder (esconde){
@@ -136,24 +179,58 @@ import Quagga from 'quagga'
         }
       },
 
-      
       baixar(){
         lista = JSON.parse(localStorage.getItem("lista"));
-        console.log(lista)
         var item = '<td>EAN<td>PRODUTO<td>VALOR';
+
+        var customers = '';
+
         for(var i = 0; i < lista.length; i++){
           item = item + '<tr>';
-          item = item + '<td>' + lista[i].ean;
-          item = item + '<td>' + lista[i].produto;
-          item = item + '<td>' + lista[i].valor;
+          item = item + '<td>' + lista[i].ean + '</td>';
+          item = item + '<td>' + lista[i].produto + '</td>';
+          item = item + '<td>' + lista[i].valor + '</td>';
+          item = item + '</tr>';
         }
-        
+
         var uri = 'data:application/vnd.ms-excel;base64,'
           , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta charset="utf-8"> </head><body><table>{table}</table></body></html>'
           , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
           , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
           var ctx = {worksheet: name || 'Planilha', table: item}
           window.location.href = uri + base64(format(template, ctx));
+      },
+
+      ver(){
+        lista = JSON.parse(localStorage.getItem("lista"));
+        var item = '<td>EAN<td>PRODUTO<td>VALOR';
+
+        var customers = '';
+
+        for(var i = 0; i < lista.length; i++){
+          item = item + '<tr>';
+          item = item + '<td>' + lista[i].ean + '</td>';
+          item = item + '<td>' + lista[i].produto + '</td>';
+          item = item + '<td>' + lista[i].valor + '</td>';
+          item = item + '</tr>';
+        }
+        customers = customers + item;
+        document.getElementById("pagina").style.display = 'none';
+        document.getElementById("excel").style.display = 'block';
+        document.getElementById("customers").innerHTML = customers;
+
+        
+      },
+
+      voltar(){
+        document.getElementById("pagina").style.display = 'block';
+        document.getElementById("excel").style.display = 'none';
+      },
+
+      removertudo(){
+        lista = [];
+        localStorage.setItem("lista", JSON.stringify(lista));
+        document.getElementById("customers").innerHTML = '<td>EAN<td>PRODUTO<td>VALOR';
       }
   }
 }
